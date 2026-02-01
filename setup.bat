8295c2d3dfb41a10f5053cf0f3ced7f836c31019
Supred <supred5022@gmail.com>
2026-02-01 17:34:55 +0200

    feat: update setup scripts and docker-compose to use environment variables for database credentials

--- setup.bat
+++ setup.bat
@@ -1,4 +1,5 @@
-docker compose up -d --build
+:: For .bat (Windows)
+docker compose --env-file .\app\.env up -d --build
 docker compose exec app composer install
 docker compose exec app php artisan key:generate
 docker compose exec app php artisan migrate
